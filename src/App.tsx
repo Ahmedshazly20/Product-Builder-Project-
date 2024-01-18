f6e3cb36bf1e1f959e14c5d4436e3845f2bc3e47
Ahmedshazly20 <Ahmed.shazly2019@gmail.com>
2024-01-18 07:12:57 +0200

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ProductCard from './component/ProductCard';
 
 function App() {
 
 
   return (
     <div className="App">
+      <ProductCard/>
        Hello from product bulilder
     </div>
   );
